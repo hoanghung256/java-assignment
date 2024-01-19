65db28079c1d3022aa1c8ce56837e6db9e22e8d2
Hoang Vu Hung <hoanghung250604@gmail.com>
2024-01-19 10:53:14 +0700

    Add models

--- LAB211-OOP with Java Lab/StudentManagement/src/model/ITStudent.java
+++ LAB211-OOP with Java Lab/StudentManagement/src/model/ITStudent.java
@@ -8,6 +8,24 @@ package model;
  *
  * @author hoang
  */
-public class ITStudent {
-    
+public class ITStudent extends Student {
+    private double javaScore;
+    private double cssScore;
+
+    public ITStudent(int Id, String fullName, Address address, double javaScore, double cssScore) {
+        super(Id, fullName, address);
+        this.javaScore = javaScore;
+        this.cssScore = cssScore;
+    }
+
+    public ITStudent(double javaScore, double cssScore, int Id, String fullName, Address address) {
+        super(Id, fullName, address);
+        this.javaScore = javaScore;
+        this.cssScore = cssScore;
+    }
+
+    @Override
+    public String toString() {
+        return "ITStudent{" + super.toString() + ", javaScore=" + javaScore + ", cssScore=" + cssScore + '}';
+    }
 }
--- LAB211-OOP with Java Lab/StudentManagement/src/model/Student.java
+++ LAB211-OOP with Java Lab/StudentManagement/src/model/Student.java
@@ -9,5 +9,42 @@ package model;
  * @author hoang
  */
 public class Student {
-    
+    private int Id;
+    private String fullName;
+    private Address address;
+
+    public Student(int Id, String fullName, Address address) {
+        this.Id = Id;
+        this.fullName = fullName;
+        this.address = address;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Id=" + Id + ", fullName=" + fullName + ", address=" + address;
+    }
 }
